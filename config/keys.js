3e8e2acc31f915e67f1ca7376ee230fbbff175dd
Rahul N <rahul.n@ge.com>
2018-05-12 23:45:16 +0530

    mongoose connect

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://rahuln:rahuln@ds017726.mlab.com:17726/devconnector'
+}
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Mongoose connect
+mongoose
+  .connect(db)
+  .then(() => console.log('Successfully connected to db'))
+  .catch(err => console.error('Error in connection to db', err));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
